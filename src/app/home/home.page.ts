8ee8a81a8d444f756aa79f4bddef2da03bd4483d
Glauber Goncalves <sistemas.glauber@gmail.com>
2020-02-01 11:10:55 -0300

    criação do stories

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public stories = [    
+    {'nome': 'jessica', 'img':'assets/usuarios/2.jpg'},
+    {'nome': 'zelia', 'img':'assets/usuarios/3.jpg'},
+    {'nome': 'wellington', 'img':'assets/usuarios/4.jpg'},
+    {'nome': 'douglas', 'img':'assets/usuarios/5.jpg'},
+    {'nome': 'luana', 'img':'assets/usuarios/6.jpg'},
+    {'nome': 'ms', 'img':'assets/usuarios/7.jpg'},
+    {'nome': 'windersonnunes', 'img':'assets/usuarios/8.jpg'}
+  ]
+
   constructor() {}
 
 }
