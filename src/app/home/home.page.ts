50f9943ddfe0a78436670132f6984d9a01af9406
Glauber Goncalves <sistemas.glauber@gmail.com>
2020-02-01 23:10:21 -0300

    feed de noticias

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,6 +17,16 @@ export class HomePage {
     {'nome': 'windersonnunes', 'img':'assets/usuarios/8.jpg'}
   ]
 
+  public posts = [
+    {'nome': 'jessica', 'img':'assets/usuarios/2.jpg', 'imgPost': 'assets/posts/post_1.jpg'},
+    {'nome': 'zelia', 'img':'assets/usuarios/3.jpg', 'imgPost': 'assets/posts/post_2.jpg'},
+    {'nome': 'wellington', 'img':'assets/usuarios/4.jpg', 'imgPost': 'assets/posts/post_3.jpg'},
+    {'nome': 'douglas', 'img':'assets/usuarios/5.jpg', 'imgPost': 'assets/posts/post_4.jpg'},
+    {'nome': 'luana', 'img':'assets/usuarios/6.jpg', 'imgPost': 'assets/posts/post_5.jpg'},
+    {'nome': 'ms', 'img':'assets/usuarios/7.jpg', 'imgPost': 'assets/posts/post_6.jpg'},
+    {'nome': 'windersonnunes', 'img':'assets/usuarios/8.jpg', 'imgPost': 'assets/posts/post_7.jpg'}
+  ]
+
   constructor() {}
 
 }
